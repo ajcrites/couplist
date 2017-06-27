b8d59efaea7f2bfdf7caf296172a6f7d46bf5767
Andrew Crites <acrites@mobiquityinc.com>
2017-06-27 16:10:07 -0400

    Adding support for HMR

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,22 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { hmrBootstrap } from './hmr';
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
+
+if (environment.hmr) {
+  if (module['hot']) {
+    hmrBootstrap(module, bootstrap);
+  }
+  else {
+    console.error('HMR is not properly configured');
+  }
+}
+else {
+  bootstrap();
+}
